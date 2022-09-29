d2232bba05a628f9a3d370e6c660247216becf53
MauIlAn <ec2-user@ip-172-31-9-91.us-east-2.compute.internal>
2022-09-29 22:22:03 +0000

    subiendo mi proyecto a GitHub desde cloud9

--- PrimosLista.py
+++ PrimosLista.py
@@ -1,1 +1,18 @@
+from pickle import TRUE
 
+def lista():
+  print("Introduce un valor para la creación de la lista")
+  X = int(input())
+  lst = list(range(1, X+1))
+  def f(n):
+      return all([not n%x==0 for x in range (2,n)])
+      
+  for i in range(1,X+1):
+      primos = []
+      for lNum in range(1, X+1):
+          if (f(lNum)):
+              primos.append(lNum)
+  print("Lista generada con el valor asignado: " + str(lst))
+  print("Lista de primos con ell valor asignaado: " + str(primos))
+    
+lista()
